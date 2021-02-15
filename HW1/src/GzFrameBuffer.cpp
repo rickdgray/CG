0cb42b9f2c95fb9dfe381172035eabfabd2162a8
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-02-15 16:26:10 -0600

    depth

--- HW1/src/GzFrameBuffer.cpp
+++ HW1/src/GzFrameBuffer.cpp
@@ -7,6 +7,7 @@ void GzFrameBuffer::initFrameSize(GzInt width, GzInt height)
 	Width = width;
 	Height = height;
 	ColorBuffer = new GzColor[Width * Height];
+	DepthBuffer = new GzReal[Width * Height];
 }
 
 GzImage GzFrameBuffer::toImage()
@@ -16,11 +17,18 @@ GzImage GzFrameBuffer::toImage()
 
 void GzFrameBuffer::clear(GzFunctional buffer)
 {
-	if (buffer == GZ_COLOR_BUFFER)
+	if (buffer & GZ_COLOR_BUFFER)
 	{
 		for (int y = 0; y < Height; y++)
 			for (int x = 0; x < Width; x++)
 				ColorBuffer[x + (y * Width)] = ClearColor;
+	}
+
+	if (buffer & GZ_DEPTH_BUFFER)
+	{
+		for (int y = 0; y < Height; y++)
+			for (int x = 0; x < Width; x++)
+				DepthBuffer[x + (y * Width)] = ClearDepth;
 	}
 }
 
@@ -31,6 +39,7 @@ void GzFrameBuffer::setClearColor(const GzColor& color)
 
 void GzFrameBuffer::setClearDepth(GzReal depth)
 {
+	ClearDepth = depth;
 }
 
 void GzFrameBuffer::drawPoint(const GzVertex& v, const GzColor& c, GzFunctional status)
