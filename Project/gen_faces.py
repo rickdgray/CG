7d039a22a6cf82ae7ab2612809f7a7bc39477e67
Rick <rickdgray@outlook.com>
2021-04-28 21:35:50 -0500

    fixed runtime errors

--- Project/gen_faces.py
+++ Project/gen_faces.py
@@ -13,9 +13,9 @@ from decalib.models.FLAME import FLAME
 from decalib.models.decoders import Generator
 
 @torch.no_grad()
-def encode(flame_encoder, images):
+def encode(flame_encoder, images, param_dict):
     parameters = flame_encoder(images)
-    codedict = decompose_code(parameters, {'shape': 100, 'tex': 50, 'exp': 50, 'pose': 6, 'cam': 3, 'light': 27})
+    codedict = decompose_code(parameters, param_dict)
     codedict['images'] = images
     return codedict
 
@@ -31,7 +31,7 @@ def decompose_code(code, num_dict):
 @torch.no_grad()
 def decode(flame_decoder, codedict, renderer):
     images = codedict['images']
-    verts, landmarks2d, landmarks3d = flame_decoder(shape_params=codedict['shape'])
+    verts, landmarks2d, landmarks3d = flame_decoder(shape_params=codedict['shape'], expression_params=codedict['exp'], pose_params=codedict['pose'])
     landmarks2d = util.batch_orth_proj(landmarks2d, codedict['cam'])[:,:,:2]
     landmarks2d[:,:,1:] = -landmarks2d[:,:,1:]
     landmarks2d = landmarks2d * 112 + 112
@@ -61,14 +61,21 @@ os.makedirs(savefolder, exist_ok=True)
 flame_config = CN()
 flame_config.flame_model_path = 'data/generic_model.pkl'
 flame_config.flame_lmk_embedding_path = 'data/landmark_embedding.npy'
+flame_config.param_list = ['shape', 'tex', 'exp', 'pose', 'cam', 'light']
 flame_config.n_shape = 100
+flame_config.n_tex = 50
 flame_config.n_exp = 50
+flame_config.n_pose = 6
+flame_config.n_cam = 3
+flame_config.n_light = 27
+
+param_dict = {i:flame_config.get('n_' + i) for i in flame_config.param_list}
 
 #load flat images
 testdata = datasets.TestData('input', face_detector='fan')
 
 #instantiate objects
-flame_encoder = ResnetEncoder(outsize=1).to('cuda') #TODO: check outsize number
+flame_encoder = ResnetEncoder(outsize=236).to('cuda')
 flame_decoder = FLAME(flame_config).to('cuda')
 renderer = SRenderY(224, obj_filename='data/head_template.obj').to('cuda')
 
@@ -77,36 +84,16 @@ checkpoint = torch.load('data/deca_model.tar')
 util.copy_state_dict(flame_encoder.state_dict(), checkpoint['E_flame'])
 flame_encoder.eval()
 
-name = testdata[0]['imagename']
-images = testdata[0]['image'].to('cuda')[None,...]
-
-codedict = encode(flame_encoder, images)
-opdict, visdict = decode(flame_decoder, codedict, renderer)
-
-os.makedirs(os.path.join(savefolder, name), exist_ok=True)
-np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'].cpu().numpy())
-np.savetxt(os.path.join(savefolder, name, name + '_kpt3d.txt'), opdict['landmarks3d'].cpu().numpy())
-
-vertices = opdict['vertices'][0].cpu().numpy()
-faces = renderer.faces[0].cpu().numpy()
-util.write_obj(os.path.join(savefolder, name, name + '.obj'), vertices, faces)
-
-for vis_name in ['inputs', 'rendered_images', 'shape_images']:
-    if vis_name not in visdict.keys():
-        continue
-    image = util.tensor2image(visdict[vis_name][0])
-    cv2.imwrite(os.path.join(savefolder, name, name + '_' + vis_name +'.jpg'), util.tensor2image(visdict[vis_name][0]))
-'''
 for i in tqdm(range(len(testdata))):
     name = testdata[i]['imagename']
     images = testdata[i]['image'].to('cuda')[None,...]
 
-    codedict = encode(flame_encoder, images)
+    codedict = encode(flame_encoder, images, param_dict)
     opdict, visdict = decode(flame_decoder, codedict, renderer)
     
     os.makedirs(os.path.join(savefolder, name), exist_ok=True)
-    np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'].cpu().numpy())
-    np.savetxt(os.path.join(savefolder, name, name + '_kpt3d.txt'), opdict['landmarks3d'].cpu().numpy())
+    np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'][0].cpu().numpy())
+    np.savetxt(os.path.join(savefolder, name, name + '_kpt3d.txt'), opdict['landmarks3d'][0].cpu().numpy())
 
     vertices = opdict['vertices'][i].cpu().numpy()
     faces = renderer.faces[0].cpu().numpy()
@@ -117,4 +104,3 @@ for i in tqdm(range(len(testdata))):
             continue
         image = util.tensor2image(visdict[vis_name][0])
         cv2.imwrite(os.path.join(savefolder, name, name + '_' + vis_name +'.jpg'), util.tensor2image(visdict[vis_name][0]))
-'''
